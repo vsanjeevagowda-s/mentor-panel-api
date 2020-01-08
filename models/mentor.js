5dfae1ec0840f054cae40d6691bbc3089c976c54
venkatesh <vsanjeevagowda@qwini.io>
2020-01-08 03:17:22 +0000

    mentor additon and deletion

--- models/mentor.js
+++ models/mentor.js
@@ -1,10 +1,13 @@
- 
+
 const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
 const mentorSchema = new Schema({
- name: { type: String },
+  full_name: { type: String },
+  email: { type: String },
+  mobile: { type: String },
+  about: { type: String },
   updated_at: { type: Date, default: Date.now },
   created_date: { type: Date, default: Date.now },
 });
--- src/api/controllers/mentor.js
+++ src/api/controllers/mentor.js
@@ -3,8 +3,8 @@ const hmve = require('hmve');
 
 const createMentor = async (req, res) => {
   try {
-    const { name, email, subject, mobile } = req.body;
-    const mentor = await Mentor.create({ name, email, subject, mobile });
+    const { fullName: full_name, email, about, mobile } = req.body;
+    const mentor = await Mentor.create({ full_name, email, about, mobile });
     const message = 'Mentor created successfully!';
     return res.status(200).json({ message, mentor });
   } catch (error) {
@@ -13,7 +13,12 @@ const createMentor = async (req, res) => {
 };
 
 const listMentor = async (req, res) => {
-
+  try {
+    const mentors = await Mentor.find();
+    return res.status(200).json({ mentors });
+  } catch (error) {
+    return res.status(422).json({ message: hmve(Mentor, error).message });
+  }
 }
 
 const updateMentor = async (req, res) => {
@@ -21,7 +26,13 @@ const updateMentor = async (req, res) => {
 }
 
 const deleteMentor = async (req, res) => {
-
+    try {
+      const { id } = req.params;
+      const mentors = await Mentor.deleteOne({ _id: id });
+      return res.status(200).json({ message: "Mentor Deleted Successfully." });
+    } catch (error) {
+      return res.status(422).json({ message: hmve(Mentor, error).message });
+    }
 }
 
 module.exports ={
